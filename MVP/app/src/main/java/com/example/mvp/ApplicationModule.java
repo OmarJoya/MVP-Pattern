c2989b540523a6e54d79363ced33f4e6ce14ce55
Omar <ojoyar@gmail.com>
2019-01-15 19:11:26 -0600

    Adding dagger2 and ApplicationModule

---
+++ MVP/app/src/main/java/com/example/mvp/ApplicationModule.java
@@ -0,0 +1,26 @@
+package com.example.mvp;
+
+import android.app.Application;
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class ApplicationModule {
+    //Dagger 2 will use this class to track dependencies
+
+    private Application application;
+
+    public ApplicationModule(Application application) {
+        this.application = application;
+    }
+
+    @Provides
+    @Singleton //instance should be created only once
+    public Context provideContext(){
+        return application;
+    }
+}
